79135092082af1fdeaa3837f08d61d249ffbbf6b
aeyalcinoglu <aeyalcinoglu@gmail.com>
2022-09-28 00:42:57 +0200

    Add wasm interactivity chapter

--- wasm-book/wasm-game-of-life/www/index.js
+++ wasm-book/wasm-game-of-life/www/index.js
@@ -65,16 +65,65 @@ const drawCells = () => {
 	ctx.stroke();
 };
 
+canvas.addEventListener("click", event => {
+	const boundingRect = canvas.getBoundingClientRect();
+
+	const scaleX = canvas.width / boundingRect.width;
+	const scaleY = canvas.height / boundingRect.height;
+
+	const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+	const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+	const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+	const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+	
+	universe.toggle_cell(row, col);
+
+	drawGrid();
+	drawCells();
+});
+
+let animationId = null;
+
+const isPaused = () => {
+	return animationId === null;
+}
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+	playPauseButton.textContent = "⏸";
+	renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+	if (isPaused()) {
+		play();
+	} else {
+		pause();
+	}
+});
+
+const ticksPerAnimationFrameInput = document.getElementById("ticks-per-animation-frame");
+
 const renderLoop = () => {
 	// debugger;
-	universe.tick();
+	for (var i = 0; i < ticksPerAnimationFrameInput.value; i++) {
+		universe.tick();
+	}
 
 	drawGrid();
 	drawCells();
 
-	requestAnimationFrame(renderLoop);
+	animationId = requestAnimationFrame(renderLoop);
 };
 
 drawGrid();
 drawCells();
-requestAnimationFrame(renderLoop);
+play();
